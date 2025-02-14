08e26a13bdd5c881db21cc19b6598c83951c515b
AnneDuff <2430060@bart.ca>
2025-02-14 13:48:31 -0500

    removed build foldeR

---
+++ labS3J2/Sources/labo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+void insert(Node* currNode, void* newData){
+
+}
+
+void insertTail(Node* head, void* newData){
+
+}
+
+void insertHead(Node* head, void* newData){
+
+}
+
+Node* removeNode(Node* currNode){
+
+	return nullptr;
+}
+
+void alphabetise(Node* head, char* names[]){
+
+}
