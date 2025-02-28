fe1bce684536bcd7afdf88fb9870d752a93bff9f
AnneDuff <2430060@bart.ca>
2025-02-28 18:56:24 -0500

    Remise labos S4J1 et S4J2

--- labS3J2/Sources/labo.c
+++ labS3J2/Sources/labo.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+typedef struct Node Node;
+
+struct Node {
+	void* data;
+	Node* prev;
+	Node* next;
+};
+
+typedef struct person_t {
+	char name[256];
+	size_t age;
+} Person;
 
 void insert(Node* currNode, void* newData){
-
+	Node n = { 0 };
+	n.data = newData;
+	n.prev = NULL;
+	n.next = NULL;
+	if (currNode->next != NULL) {
+		n.next = currNode->next;
+		currNode->next->prev = n;
+	}
+	currNode->next = n;
+	n.prev = currNode;
 }
 
 void insertTail(Node* head, void* newData){
@@ -15,7 +36,7 @@ void insertHead(Node* head, void* newData){
 
 Node* removeNode(Node* currNode){
 
-	return nullptr;
+	return NULL;
 }
 
 void alphabetise(Node* head, char* names[]){
